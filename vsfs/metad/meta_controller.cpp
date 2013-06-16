82ad5c473ce711f25defd254639893869f1112ea
Ziling Huang <hzlgis@gmail.com>
2013-06-16 15:36:44 -0500

    * imported meta_controller

---
+++ vsfs/metad/meta_controller.cpp
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2013 (c) Ziling Huang <hzlgis@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <boost/shared_ptr.hpp>
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <protocol/TBinaryProtocol.h>
+#include <server/TThreadedServer.h>
+#include <string.h>
+#include <transport/TServerSocket.h>
+#include <transport/TTransportUtils.h>
+#include <unistd.h>
+#include <condition_variable>
+#include <chrono>
+#include <string>
+#include <thread>
+#include <vector>
+// #include "vsfs/client/vsfs_rpc_client.h"
+#include "vsfs/rpc/MasterDaemon.h"
+#include "vsfs/metad/meta_server.h"
+#include "vsfs/metad/meta_controller.h"
+
+using apache::thrift::TProcessor;
+using apache::thrift::protocol::TBinaryProtocol;
+using apache::thrift::protocol::TBinaryProtocolFactory;
+using apache::thrift::protocol::TProtocolFactory;
+using apache::thrift::transport::TBufferedTransportFactory;
+using apache::thrift::transport::TServerSocket;
+using apache::thrift::transport::TServerTransport;
+using apache::thrift::transport::TTransportFactory;
+using boost::shared_ptr;
+using std::string;
+using std::vector;
+using vsfs::rpc::NodeAddressList;
+using vsfs::rpc::NodeInfo;
+
+DEFINE_string(host, "", "Manually set the hostname of this MetaManager.");
+DEFINE_int32(port, 6666, "Set the listen port of MetaManager.");
+DEFINE_string(server_id, "", "Set the server ID.");
+DEFINE_string(master_addr, "localhost", "Set the address of the master"
+              "node.");
+DEFINE_int32(master_port, 9876, "Set the port of the master node.");
+
+namespace vsfs {
+
+// using client::VSFSRpcClient;
+
+namespace metad {
+
+MetaManager::MetaManager()
+    : host_(FLAGS_host), port_(FLAGS_port), manager_(new MetaManager)
+      // ,master_(new VSFSRpcClient::MasterClientType(FLAGS_master_addr,
+      //                                            FLAGS_master_port))
+                                                  { // NOLINT
+}
+
+MetaManager::~MetaManager() {
+  stop();
+}
+
+void MetaManager::background_task() {
+  LOG(INFO) << "Joining master: " << FLAGS_master_addr
+            << ":" << FLAGS_master_port;
+  join();
+
+  // TODO(Ziling): background tasks
+}
+
+void MetaManager::start() {
+  shared_ptr<MetaManagerHandler> handler(new MetaManagerHandler(this));
+  shared_ptr<TProcessor> processor(new MetaManagerProcessor(handler));
+  shared_ptr<TProtocolFactory> protocol_factory(new TBinaryProtocolFactory());
+  shared_ptr<TServerTransport> server_transport(
+      new TServerSocket(port_));
+  shared_ptr<TTransportFactory> transport_factory(
+      new TBufferedTransportFactory());
+  server_.reset(new TThreadedServer(processor, server_transport,
+                                    transport_factory, protocol_factory));
+  LOG(INFO) << "Starting MetaManager at port:" << port_;
+  background_thread_ = thread(&MetaManager::background_task, this);
+  server_->serve();
+  LOG(INFO) << "Shutting down completely.";
+}
+
+void MetaManager::stop() {
+  LOG(INFO) << "Attempts to shutdown...";
+  if (server_) {
+    server_->stop();
+  }
+  if (background_thread_.joinable()) {
+    background_cv_.notify_all();
+    background_thread_.join();
+  }
+}
+
+Status MetaManager::join() {
+  // CHECK_NOTNULL(master_.get());
+  // master_->open();
+  NodeInfo node_info;
+
+  if (host_.empty()) {
+    char host_buffer[HOST_NAME_MAX];
+    int ret = gethostname(host_buffer, HOST_NAME_MAX);
+    if (ret) {
+      return Status(ret, strerror(ret));
+    }
+    node_info.address.host = host_buffer;
+  } else {
+    node_info.address.host = host_;
+  }
+  node_info.address.port = port_;
+  node_info.server_id = FLAGS_server_id;
+
+  NodeAddressList replica_server_addresses;
+  // master_->handler()->join_meta_server(replica_server_addresses, node_info);
+
+  // TODO(Ziling): join replica and LOG INFO
+  return Status::OK;
+}
+
+Status MetaManager::insert(uint64_t file_id, const string &file_path) {
+  return manager_->insert(file_id, file_path);
+}
+
+Status MetaManager::insert(const RpcMetaDataList& metadata) {
+  return manager_->insert(metadata);
+}
+
+Status MetaManager::remove(uint64_t file_id) {
+  return manager_->remove(file_id);
+}
+
+Status MetaManager::find(uint64_t file_id, string *file_path) {
+  return manager_->find(file_id, file_path);
+}
+
+Status MetaManager::find(const vector<int64_t>& file_ids,
+                        vector<string>* results) {
+  return manager_->find(file_ids, results);
+}
+
+}  // namespace metad
+}  // namespace vsfs
---
+++ vsfs/metad/meta_controller.h
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2013 (c) Ziling Huang <hzlgis@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VSFS_METAD_META_CONTROLLER_H_
+#define VSFS_METAD_META_CONTROLLER_H_
+
+#include <boost/shared_ptr.hpp>
+#include <server/TThreadedServer.h>
+#include <condition_variable>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
+#include "vobla/status.h"
+// #include "vsfs/client/vsfs_rpc_client.h"
+#include "vsfs/rpc/MasterDaemon.h"
+#include "vsfs/metad/meta_manager.h"
+
+using apache::thrift::server::TThreadedServer;
+using boost::shared_ptr;
+using std::condition_variable;
+using std::mutex;
+using std::thread;
+using std::string;
+using std::unique_ptr;
+using std::vector;
+
+namespace vsfs {
+namespace metad {
+
+class MetaController {
+ public:
+  MetaController();
+
+  virtual ~MetaController();
+
+  /// Start this server.
+  void start();
+
+  /// Stop this server.
+  void stop();
+
+  /// Join this Meta Server to the CH ring in master.
+  Status join();
+
+  /**
+   * \brief Insert a record into key-value storage.
+   * \param[in] file_id The unique id of the file.
+   * \param[in] file_path The path of the file.
+   */
+  virtual Status insert(uint64_t file_id, const string &file_path);
+
+  /**
+   * \brief Insert a bunch of <file_id, file_path> pair.
+   */
+  virtual Status insert(const RpcMetaDataList& metadata);
+
+  /**
+   * \brief Remove a record from the key-value storage.
+   * \param[in] file_id The unique id of the file.
+   */
+  virtual Status remove(uint64_t file_id);
+
+  /**
+   * \brief Given a file_id, return the corresponding file_path.
+   * \param[in] file_id The unique id of the file.
+   * \param[out] The path of the file.
+   */
+  virtual Status find(uint64_t file_id, string *file_path);
+
+  virtual Status find(const vector<int64_t>& file_ids,
+                      vector<string>* results);
+
+  /* MasterDaemonClient* master_connection() const {
+    return master_->handler();
+  } */
+
+ private:
+  string host_;
+
+  /// Listen port.
+  int port_;
+
+  /// Thrift RPC server.
+  shared_ptr<TThreadedServer> server_;
+
+  /// Meta Manager instance.
+  unique_ptr<MetaManager> manager_;
+
+  /// Connection to master
+  // unique_ptr<client::VSFSRpcClient::MasterClientType> master_;
+
+  void background_task();
+
+  thread background_thread_;
+
+  condition_variable background_cv_;
+
+  mutex back_ground_mutex_;
+};
+
+}  // namespace metad
+}  // namespace vsfs
+
+#endif  // VSFS_METAD_META_CONTROLLER_H_
