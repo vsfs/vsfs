863ade89ecf204f7e50991661dc799339e2b0d86
Lei Xu <eddyxu@gmail.com>
2013-08-03 20:27:38 -0500

    + Added a class to set up a masterd cluster for test.

---
+++ vsfs/masterd/testing/test_masterd_cluster.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2013 (c) Lei Xu <eddyxu@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <string>
+#include "vsfs/masterd/testing/test_masterd_cluster.h"
+
+namespace vsfs {
+namespace masterd {
+namespace testing {
+
+TestMasterdCluster::TestMasterdCluster(const string& dirpath, int nmasterd)
+    : basedir_(dirpath), num_masterds_(nmasterd) {
+}
+
+TestMasterdCluster::~TestMasterdCluster() {
+  stop();
+}
+
+void TestMasterdCluster::start() {
+  const int kPrimaryPort = 10100;
+  cluster_.emplace_back(unique_ptr<MasterController>(
+          new MasterController(basedir_, "localhost", kPrimaryPort, true)));
+  threads_.emplace_back(
+      thread(&MasterController::start, cluster_.back().get()));
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+
+  // Starts the secondary masters.
+  for (int i = 1; i < num_masterds_; ++i) {
+    cluster_.emplace_back(unique_ptr<MasterController>(
+            new MasterController(basedir_, "localhost", kPrimaryPort + i,
+                                 false, "localhost", kPrimaryPort)));
+    threads_.emplace_back(
+        thread(&MasterController::start, cluster_.back().get()));
+  }
+}
+
+void TestMasterdCluster::stop() {
+  /// Stops the cluster and wait all threads exit.
+  for (auto it = cluster_.rbegin(); it != cluster_.rend(); ++it) {
+    (*it)->stop();
+  }
+  for (auto& thd : threads_) {
+    if (thd.joinable()) {
+      thd.join();
+    }
+  }
+}
+
+}  // namespace testing
+}  // namespace masterd
+}  // namespace vsfs
---
+++ vsfs/masterd/testing/test_masterd_cluster.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2013 (c) Lei Xu <eddyxu@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VSFS_MASTERD_TESTING_TEST_MASTERD_CLUSTER_H_
+#define VSFS_MASTERD_TESTING_TEST_MASTERD_CLUSTER_H_
+
+#include <boost/utility.hpp>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+#include "vobla/status.h"
+#include "vsfs/masterd/master_controller.h"
+
+using std::string;
+using std::thread;
+using std::unique_ptr;
+using std::vector;
+using vobla::Status;
+
+namespace vsfs {
+namespace masterd {
+namespace testing {
+
+/**
+ * \brief Manages a test masterd cluster for test purpose.
+ */
+class TestMasterdCluster : boost::noncopyable {
+ public:
+  /**
+   * \brief Constructs a Masterd cluster with 'num_masterd' nodes. The first
+   * node will be the primary one.
+   */
+  TestMasterdCluster(const string& basedir, int num_masterd);
+
+  /// Destructs the cluster and stop / reclaim resources.
+  virtual ~TestMasterdCluster();
+
+  /// Starts the cluster.
+  void start();
+
+  /// Stops the cluster.
+  void stop();
+
+ private:
+  string basedir_;
+
+  int num_masterds_;
+
+  vector<unique_ptr<MasterController>> cluster_;
+
+  /// Threads to run MasterController.
+  vector<thread> threads_;
+};
+
+}  // namespace testing
+}  // namespace masterd
+}  // namespace vsfs
+
+#endif  // VSFS_MASTERD_TESTING_TEST_MASTERD_CLUSTER_H_
