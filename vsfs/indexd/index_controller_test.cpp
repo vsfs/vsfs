1b2a17a47ce34ffa6e93ad0c2b67d6647c5a460c
Ziling Huang <hzlgis@gmail.com>
2013-09-02 22:11:21 -0500

    * init file

---
+++ vsfs/indexd/index_controller_test.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2013 (c) Ziling Huang <hzlgis@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <memory>
+#include <string>
+#include <vector>
+#include "vobla/file.h"
+#include "vobla/status.h"
+#include "vsfs/indexd/index_controller.h"
+#include "vsfs/rpc/mock_rpc_clients.h"
+#include "vsfs/rpc/vsfs_types.h"
+
+using ::testing::_;
+using std::string;
+using std::to_string;
+using std::unique_ptr;
+using std::vector;
+using vobla::Status;
+using vsfs::rpc::MockMasterServerClient;
+using vsfs::rpc::TestRpcClientFactory;
+
+namespace vsfs {
+namespace indexd {
+
+class IndexControllerTest : public ::testing::Test {
+  typedef RpcClient<MasterServerClient, TFramedTransport> ClientType;
+ protected:
+  void SetUp() {
+    shared_ptr<MockMasterServerClient> mock_master_(new MockMasterServerClient);
+    unique_ptr<MasterServerClient> master(new MasterServerClient(mock_master_));
+
+    tmpdir_.reset(new vobla::TemporaryDirectory);
+    controller_.reset(new IndexController(tmpdir_->path(), "", 9876,
+                                          master.release()));
+  }
+
+  void TearDown() {
+  }
+
+  unique_ptr<IndexController> controller_;
+  MockMasterServerClient* mock_master_;
+  unique_ptr<vobla::TemporaryDirectory> tmpdir_;
+};
+
+TEST_F(IndexControllerTest, TestInsert) {
+}
+
+}  // namespace indexd
+}  // namespace vsfs
