6809f9f22fe3f9529f0d8a8d2191949962d3a810
Ziling Huang <hzlgis@gmail.com>
2013-09-04 16:43:22 -0500

    * added more test case.

--- vsfs/indexd/index_controller_test.cpp
+++ vsfs/indexd/index_controller_test.cpp
@@ -31,6 +31,7 @@ using std::to_string;
 using std::unique_ptr;
 using std::vector;
 using vobla::Status;
+using vsfs::RpcComplexQuery;
 using vsfs::rpc::MockMasterServerClient;
 using vsfs::rpc::TestRpcClientFactory;
 
@@ -60,6 +61,88 @@ class IndexControllerTest : public ::testing::Test {
 TEST_F(IndexControllerTest, TestCreateIndex) {
   EXPECT_TRUE(controller_->create_index("/foo/bar", "test", IndexInfo::BTREE,
                                         UINT64).ok());
+}
+
+TEST_F(IndexControllerTest, TestUpdate) {
+  EXPECT_TRUE(controller_->create_index("/foo/bar", "test", IndexInfo::BTREE,
+                                        UINT64).ok());
+  RpcIndexUpdate updates;
+  updates.txn_id = 100;
+  updates.updates.emplace_back();
+  updates.updates.back().root_path = "/foo/bar";
+  updates.updates.back().name = "test";
+  for (int i = 0; i < 100; i++) {
+    updates.updates.back().record_updates.emplace_back();
+    auto& record = updates.updates.back().record_updates.back();
+    record.op = RpcIndexUpdateOpCode::UPDATE;
+    record.key = std::to_string(i);
+    record.value = std::to_string(i);
+  }
+  EXPECT_TRUE(controller_->update(updates).ok());
+}
+
+TEST_F(IndexControllerTest, TestSearchAfterUpdate) {
+  EXPECT_TRUE(controller_->create_index("/foo/bar", "test", IndexInfo::BTREE,
+                                        UINT64).ok());
+  RpcIndexUpdate updates;
+  updates.txn_id = 100;
+  updates.updates.emplace_back();
+  updates.updates.back().root_path = "/foo/bar";
+  updates.updates.back().name = "test";
+  for (int i = 0; i < 100; i++) {
+    updates.updates.back().record_updates.emplace_back();
+    auto& record = updates.updates.back().record_updates.back();
+    record.op = RpcIndexUpdateOpCode::UPDATE;
+    record.key = std::to_string(i);
+    record.value = std::to_string(i);
+  }
+  EXPECT_TRUE(controller_->update(updates).ok());
+
+  vector<int64_t> obj_ids;
+  RpcComplexQuery query;
+  query.txn_id = 101;
+  query.root = "/foo/bar";
+  query.range_queries.emplace_back();
+  auto& range = query.range_queries.back();
+  range.index_path = "/foo/bar";
+  range.name = "test";
+  range.lower = "50";
+  range.lower_open = true;
+
+  EXPECT_TRUE(controller_->search(query, &obj_ids).ok());
+  EXPECT_EQ(49u, obj_ids.size());
+}
+
+TEST_F(IndexControllerTest, TestInfo) {
+  EXPECT_TRUE(controller_->create_index("/foo/bar", "energy", IndexInfo::BTREE,
+                                        UINT64).ok());
+  EXPECT_TRUE(controller_->create_index("/foo/zoo", "lumi", IndexInfo::BTREE,
+                                        UINT64).ok());
+  RpcIndexUpdate updates;
+  updates.txn_id = 100;
+  updates.updates.emplace_back();
+  updates.updates.back().root_path = "/foo/bar";
+  updates.updates.back().name = "test";
+  for (int i = 0; i < 100; i++) {
+    updates.updates.back().record_updates.emplace_back();
+    auto& record = updates.updates.back().record_updates.back();
+    record.op = RpcIndexUpdateOpCode::UPDATE;
+    record.key = std::to_string(i);
+    record.value = std::to_string(i);
+  }
+  EXPECT_TRUE(controller_->update(updates).ok());
+
+  RpcIndexInfoRequest request;
+  request.txn_id = 101;
+  request.path = "/foo/bar";
+
+  RpcIndexInfo result;
+  EXPECT_TRUE(controller_->info(request, &result).ok());
+  EXPECT_EQ("/foo/bar", result.path);
+  EXPECT_EQ("energy", result.name);
+  EXPECT_EQ(IndexInfo::BTREE, result.type);
+  EXPECT_EQ(UINT64, result.key_type);
+  EXPECT_EQ(100u, result.num_records);
 }
 
 }  // namespace indexd
