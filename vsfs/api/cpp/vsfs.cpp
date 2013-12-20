cd7d6d7e981e4efe944dd7dd2146b9fb6e4326d4
Lei Xu <eddyxu@gmail.com>
2013-12-20 20:46:24 +0000

    * Added missing vsfs.cpp

---
+++ vsfs/api/cpp/vsfs.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2014 (c) Lei Xu <eddyxu@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <glog/logging.h>
+#include <string>
+#include "vobla/status.h"
+#include "vsfs/api/cpp/vsfs.h"
+#include "vsfs/api/cpp/vsfs_impl.h"
+
+using std::string;
+using vobla::Status;
+
+namespace vsfs {
+
+using api::VsfsImpl;
+
+Vsfs::Vsfs(const string& host, int port) : impl_(new VsfsImpl(host, port)) {
+}
+
+Vsfs::Vsfs(VsfsImpl* vsfs_impl) : impl_(vsfs_impl) {
+}
+
+Vsfs::~Vsfs() {
+}
+
+Status Vsfs::connect() {
+  return impl_->connection()->init();
+}
+
+Status Vsfs::create(const string& path, int64_t mode, int64_t uid,
+                    int64_t gid, ObjectId* id) {
+  CHECK_NOTNULL(id);
+  return impl_->connection()->create(path, mode, uid, gid, id);
+}
+
+Status Vsfs::mkdir(const string& path, int64_t mode, int64_t uid, int64_t gid) {
+  return impl_->connection()->mkdir(path, mode, uid, gid);
+}
+
+}  // namespace vsfs
