9034f9dcbd48877b9c26a591ba00ec0e7c676fef
Lei Xu <eddyxu@gmail.com>
2014-01-09 19:34:19 +0000

    * move IndexCommand interface to header file

--- vsfs/ui/cli/commands.h
+++ vsfs/ui/cli/commands.h
@@ -18,20 +18,26 @@
 #define VSFS_UI_CLI_COMMANDS_H_
 
 #include <boost/utility.hpp>
-#include <string>
 #include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 using std::string;
 using std::unique_ptr;
+using std::unordered_map;
+using std::vector;
 
 namespace vobla {
-class Timer;
 class Status;
+class Timer;
 }
 
 namespace vsfs {
 namespace ui {
 namespace cli {
+
+using vobla::Status;
 
 /**
  * \class Command
@@ -90,6 +96,93 @@ class HelpCommand : public Command {
  private:
   string sub_command_;
 };
+
+/**
+ * \class IndexCommand
+ * \brief Provides an indexing interface to command line.
+ */
+class IndexCommand : public Command {
+ public:
+  IndexCommand();
+
+  int parse_args(int argc, char* const argv[]);
+
+  /// Prints the detailed help for "index" command.
+  void print_help() const;
+
+  Status run();
+
+ private:
+  /**
+   * \brief A hash mapping from the file name to a vector of indexing keys.
+   *
+   * E.g. <file name, [key0, key1, key2, ...]>
+   */
+  typedef unordered_map<string, vector<string>> IndexDataMap;
+
+  enum IndexOp {
+    /// Adds to an index entry
+    ADD = 1,
+    /// Updates an index entry.
+    UPDATE,
+    /// Deletes from an index entry.
+    DELETE
+  };
+
+  enum Operation {
+    /// Unknown operation
+    UNKNOWN,
+    /// Creates an index.
+    CREATE,
+    /// Destroy an index.
+    DESTROY,
+    /// Inserts or update records.
+    INSERT,
+    /// Remove records.
+    REMOVE,
+    /// Gets the stat of index
+    STAT,
+    /// List the indices
+    LIST,
+  };
+
+  Status create_index();
+
+  Status destroy_index();
+
+  Status update_index();
+
+  Status stat_index();
+
+  Status remove_records();
+
+  Status list_index();
+
+  string index_root_;
+
+  /// The name of named index.
+  string index_name_;
+
+  /// Set to true to use stdin to feed.
+  bool use_stdin_;
+
+  /// Operations.
+  int operation_;
+
+  /// Operations on index.
+  int index_op_;
+
+  /// The data structure type of index. (e.g., btree or hash).
+  int index_type_;
+
+  /// The type of the key of index. (e.g., int32, float or string).
+  int key_type_;
+
+  uint64_t batch_size_;
+
+  IndexDataMap index_data_;
+};
+
 
 }  // namespace cli
 }  // namespace ui
